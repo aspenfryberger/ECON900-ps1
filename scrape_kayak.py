527ca967ac3f1a853ade713295cc4be6eed85ae2
Aspen Fryberger <aspenfryberger@gmail.com>
2019-04-05 22:13:58 -0400

    adding folder to git

--- scrape_kayak.py
+++ scrape_kayak.py
@@ -4,53 +4,47 @@ Created on Thu Apr  4 12:34:45 2019
 
 @author: Aspen
 """
-
+import time
 import selenium   #not sure if I need this 
 from selenium import webdriver
 import os
 import pandas as pd
+from random import*
+from sklearn.utils import shuffle
 
+start_time = time.time()
 os.chdir("C:\\Users\\Aspen\\Documents\\Toms class\\project_1")
-codes=pd.read_excel("codes.xlsx")
 
-codes=codes['name'].str.split(",", expand=True)
-codes.columns=['one', 'two', 'three']
-names=codes[['one']]
-codes=codes['two'].str.split(" ", expand=True)
-codes.columns=['one', 'two', 'three', 'other', 'other2', 'others', 'other4']
-codes['three']=codes['three'].str[1:4]
-code2=codes[['three']
-codes3=names.join(code2)
-codes3=codes3.astype(str)
-codes3.to_csv('airport_codes.csv')
+code=pd.read_excel("codes.xlsx")  #this has airport codes
+date=pd.read_csv("date.csv")  #this is a data frame of dates
+date['month']=date['month'].map("{:02}".format)
+date['day']=date['day'].map("{:02}".format)
+code=shuffle(code).reset_index()
+code=code.drop(["index"], axis=1)
+date=shuffle(date).reset_index()
+date=date.drop(["index"], axis=1)
 
-#############
+for j in range(0,len(date)):
+    a=date.iloc[j,0]
+    b=date.iloc[j,1]
+    r=randint(1, 10)
+    time.sleep(2+r)
+    driver = webdriver.Firefox(executable_path = 'C:/Users/Aspen/Documents/EV/code/EV_scrape/geckodriver')
+    for i in range(0,len(code)):
+        c=code.iloc[i, 1]
+        r=randint(1, 10)
+        time.sleep(3+r)
+        driver.get('https://www.kayak.com/flights/ATL-'+c+'/2019-'+a+'-'+b+'?sort=bestflight_a')
+        time.sleep(3+r)
+        html = driver.page_source
+        f=open("ATL-"+c+"_"+a+"-"+b+".html","w", encoding='utf8')
+        f.write(html)
+        f.close() 
+        r=randint(1, 80)
+        time.sleep(60+r)
+    driver.close()   
+print("--- %s seconds ---" % (time.time() - start_time))
 
-code=pd.read_excel("codes.xlsx")
-driver = webdriver.Firefox(executable_path = 'C:/Users/Aspen/Documents/EV/code/EV_scrape/geckodriver')
-i=5
-a=code.iloc[i, 1]
-driver.get('https://www.kayak.com/flights/ATL-'+a+'/2019-04-05/2019-05-07?sort=bestflight_a')
-#driver.get('https://www.kayak.com/flights/ATL-DEN/2019-04-05/2019-05-07?sort=bestflight_a')
-html = driver.page_source
-
-f=open("test.html","w", encoding='utf8')
-f.write(html)
-f.close() 
-
-
-
-
-
-
-
-
-
-
-
-
-############################
-f = open("test.html", "r")
 
 
 
